1ef528a3691696d102298a16c06c75d8597e7dad
Nikolay <53932721+vavilovnv@users.noreply.github.com>
2022-04-07 01:37:01 +0300

    Промежуточный коммит

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -28,11 +28,6 @@ def index(request):
 def group_posts(request, slug):    
     template = 'posts/group_list.html'
     text = 'Здесь будет информация о группах проекта Yatube'
-
-    # Функция get_object_or_404 получает по заданным критериям объект 
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
